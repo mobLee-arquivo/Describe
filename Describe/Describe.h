a1d7638b239efef4088411a4a870bc45a3e111be
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2015-11-16 18:16:24 -0200

    Initial commit

---
+++ Describe/Describe.h
@@ -0,0 +1,19 @@
+//
+//  Describe.h
+//  Describe
+//
+//  Created by Guilherme Pacheco on 11/16/15.
+//  Copyright © 2015 mobLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Describe.
+FOUNDATION_EXPORT double DescribeVersionNumber;
+
+//! Project version string for Describe.
+FOUNDATION_EXPORT const unsigned char DescribeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Describe/PublicHeader.h>
+
+
